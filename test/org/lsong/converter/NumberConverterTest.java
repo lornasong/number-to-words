608e177b75cd8201403231e7b95f1deea3e3343b
lornasong <lorna.song@gmail.com>
2014-11-20 22:08:12 -0500

    Add shell for test
     - Number converter

---
+++ test/org/lsong/converter/NumberConverterTest.java
@@ -0,0 +1,14 @@
+package org.lsong.converter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberConverterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
